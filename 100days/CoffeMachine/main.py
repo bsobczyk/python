2a70cd14be1831b4527915d9c0e6b6f81ec16e55
Bartosz Sobczyk <bartosz.sobczyk@ing.com>
2025-02-01 17:45:21 +0100

    update

--- 100days/CoffeMachine/main.py
+++ 100days/CoffeMachine/main.py
@@ -29,43 +29,72 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+profit=0
+is_on = True
 
-
-
-
+while is_on:
 #TODO: 1. Print request_port
-user_choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+    user_choice = (input("What would you like? (espresso/latte/cappuccino):").lower()).replace(' ','')
 
-#TODO: 2. Check resource sufficient ?
-#Helper
-print("resources is:\n")
-print(f'water: {resources["water"]}')
-print(f'milk: {resources["milk"]}')
-print(f'coffe: {resources["coffee"]}')
+    if user_choice == "off":
+        is_on = False
+        break
+    if user_choice == "report":
+        print("resources is:\n")
+        print(f'water: {resources["water"]}')
+        print(f'milk: {resources["milk"]}')
+        print(f'coffe: {resources["coffee"]}')
+        print(f'Money: ${profit}')
 
-def check_resource(item):
-    if resources["water"] < MENU[item]["ingredients"]["water"]:
-        return "Sorry there is not enough water."
-    elif resources["milk"] < MENU[item]["ingredients"]["milk"]:
-        return "Sorry there is not enough milk."
-    elif resources["coffee"] < MENU[item]["ingredients"]["coffee"]:
-        return "Sorry there is not enough coffee."
+    #TODO: 2. Check resource sufficient ?
+    #Helper
+
+    def check_resource(item):
+        if resources["water"] < MENU[item]["ingredients"]["water"]:
+            return "Sorry there is not enough water."
+        elif resources["milk"] < MENU[item]["ingredients"]["milk"]:
+            return "Sorry there is not enough milk."
+        elif resources["coffee"] < MENU[item]["ingredients"]["coffee"]:
+            return "Sorry there is not enough coffee."
+        else:
+            return True
+
+
+    #TODO: 3. Process coin
+
+    def insert_coins():
+        print("Please insert coins\n")
+        quarters = int(input("How many quarters?:"))
+        dimes = int(input("How many dimes?:"))
+        nickles = int(input("How many nickles?:"))
+        pennies = int(input("How many pennies?:"))
+        pocket = {
+            'quarters' : quarters,
+            'dimes' : dimes,
+            'nickles' : nickles,
+            'pennies' : pennies
+        }
+        return pocket
+    def calculate(q,d,n,p):
+        calculation = (q * 0.25) + ( d * 0.10) + (n * 0.05) + (p *0.01)
+        return round(calculation,2)
+
+    user_coins = insert_coins()
+    user_money = calculate(user_coins['quarters'],user_coins['dimes'],user_coins['nickles'],user_coins['pennies'])
+
+
+    #TODO: 4. Check transaction successful ?
+    if user_money < MENU[user_choice]["cost"]:
+        print("Sorry that's not enough money. Money refunded.")
     else:
-        return True
+        profit = profit + MENU[user_choice]["cost"]
+        change = round((user_money - profit),2)
+        print(f'proif is: {profit}')
 
+    #TODO: 5. Make coffe
+    resources["water"] -=MENU[user_choice]["ingredients"]["water"]
+    resources["coffee"] -=MENU[user_choice]["ingredients"]["coffee"]
+    if user_choice != "espresso":
+        resources["milk"] -=MENU[user_choice]["ingredients"]["milk"]
 
-#TODO: 3. Process coin
-print("Please insert coins\n")
-quarters = int(input("How many quarters?:"))
-dimes = int(input("How many dimes?:"))
-nickles = int(input("How many nickles?:"))
-pennies = int(input("How many pennies?:"))
-def calculate(q,d,n,p):
-    calculation = (q * 0.25) + ( d * 0.10) + (n * 0.05) + (p *0.01)
-    return round(calculation,2
-
-
-print(calculate(quarters,dimes,nickles,pennies))
-#TODO: 4. Check transaction successful ?
-#TODO: 5. Make coffe
-
+    print(f'Here is your ☕ {user_choice}. Enjoy!')
