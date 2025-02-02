6da5e05ad741817279f571b70a58068894518115
Bartosz Sobczyk <bartosz.sobczyk@ing.com>
2025-02-02 19:30:10 +0100

    pdf

---
+++ 100days/Turtle/main.py
@@ -0,0 +1,11 @@
+import turtle
+from turtle import Turtle, Screen
+
+zolw = Turtle()
+zolw.shape("turtle")
+zolw.color("green")
+zolw.forward(100)
+my_screen = Screen()
+
+my_screen.exitonclick()
+
---
+++ 100days/Turtle/table.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+print(table)
+table.add_column("Pokemon Name",["Ruddy","Pikachu","sorio"])
+table.add_column("Pokemon Type",["water","Dicker","sorio"])
+table.align
+print(table)
